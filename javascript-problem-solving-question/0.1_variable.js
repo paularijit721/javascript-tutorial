f5e5848011c14d2fe2899b5868164644df35f5a3
paularijit72 <paularijit741@gmail.com>
2025-04-02 07:53:51 +0530

    first commit

---
+++ javascript-problem-solving-question/0.1_variable.js
@@ -0,0 +1,10 @@
+const account_id = 14443
+let accountEmail = "arijit@google.com"
+var accountPassword = "12345"
+accountCity = "Berlin"
+let accountState;
+
+
+// account_id = 2
+console.log(account_id);
+console.table([account_id, accountEmail, accountPassword, accountCity, accountState])
---
+++ javascript-problem-solving-question/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all JS code as newer varsion
+
+let name = "Arijit"
+let age = 25
+let isLoggedIn = false
+
+
+// numer => 2 to 53
+// bigint 
+// string => "Arijit"
+// boolean => true/false
+// symbol => unique value
+// null = standalone value
+// undefined = variable is declared but not assigned any value 
+
+// object
+console.log(typeof "Arijit");
