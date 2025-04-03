21c921c3c0ca7f01b21b28c263594ffc89f2fe1e
paularijit72 <paularijit741@gmail.com>
2025-04-03 06:59:30 +0530

    new changes

---
+++ javascript-problem-solving-question/03_conversionOperatin.js
@@ -0,0 +1,15 @@
+let score = 33
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+let isLoggedIn = "Arijit";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn); 
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
